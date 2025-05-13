721507fc100e40856c451a10a4896c03da873b74
MarcoGamer304 <marcoeli30@gmail.com>
2025-05-13 14:51:35 -0600

    subdivide index responsabilities

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,14 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
-import dotenv from "dotenv";
 import http from "http";
-import helmet from "helmet";
-import bodyParser from "body-parser";
-import compression from "compression";
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
+import app from "./app/appConfig";
 import { config } from "./utilities/config";
-import { Server } from "socket.io";
-import { sequelize } from "./database/connection";
-import { limiter } from "./utilities/limiter";
-import router from "./routes/routes";
+import { initDB } from "./database/init";
+import { initSocket } from "./sockets/socket.io";
 
-dotenv.config();
-
-const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: config.CORS_ORIGIN,
-    methods: ["GET, POST"],
-  },
-});
 
-app.use(helmet());
-app.use(cors({ origin: config.CORS_ORIGIN, credentials: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(compression());
-app.use(morgan("combined"));
-app.use(cookieParser());
-app.use(limiter);
+initDB();
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("successfully connected");
-    return sequelize.sync({ alter: true });
-  })
-  .catch((err) => console.error("err:", err));
-
-io.on("connection", (socket) => {
-  console.log(socket.id);
-
-  socket.on("message", (data) => {
-    console.log("Mensaje recibido:", data);
-    socket.emit("respuesta", "¡Mensaje recibido!");
-  });
-});
-
-app.use("/api", router);
-
-app.get("/", (req, res) => {
-  res.send("api rest wsocket");
-});
+const io = initSocket(server, config.CORS_ORIGIN);
 
 const port = process.env.PORT || 3000;
 
--- src/sockets/socket.io.ts
+++ src/sockets/socket.io.ts
@@ -1,2 +1,21 @@
+import { Server } from "socket.io";
 
+export const initSocket = (server: any, origin: string): Server => {
+  const io = new Server(server, {
+    cors: {
+      origin: origin,
+      methods: ["GET, POST"],
+    },
+  });
 
+  io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("message", (data) => {
+      console.log("Mensaje recibido:", data);
+      socket.emit("respuesta", "¡Mensaje recibido!");
+    });
+  });
+
+  return io;
+};
